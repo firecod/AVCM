25a77de415a68075e451e75e9ef5d3566e58b50f
Calcanas <43923999+Calcanas@users.noreply.github.com>
2019-02-19 00:35:26 -0600

    Update Venta.java

--- AVCM/src/model/Venta.java
+++ AVCM/src/model/Venta.java
@@ -6,9 +6,84 @@
 package model;
 
 /**
- *
- * @author diegg
+ * Version: 1 
+ * @author Adrian Israel Calcanas Ramírez
+ * Fecha: 17 - 02 - 2019
+ * Descripción: Modelado de clase Venta
  */
 public class Venta {
+    private String fechaVenta;
+    private int idProducto;
+    private int idVendedor;
+    private int idAlmacen;
+    private int idCliente;
+    private float importe;
+    private String facturaExpendida;
+    private int numeroTicket;
+
+    public String getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(String fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public int getIdAlmacen() {
+        return idAlmacen;
+    }
+
+    public void setIdAlmacen(int idAlmacen) {
+        this.idAlmacen = idAlmacen;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public float getImporte() {
+        return importe;
+    }
+
+    public void setImporte(float importe) {
+        this.importe = importe;
+    }
+
+    public String getFacturaExpendida() {
+        return facturaExpendida;
+    }
+
+    public void setFacturaExpendida(String facturaExpendida) {
+        this.facturaExpendida = facturaExpendida;
+    }
+
+    public int getNumeroTicket() {
+        return numeroTicket;
+    }
+
+    public void setNumeroTicket(int numeroTicket) {
+        this.numeroTicket = numeroTicket;
+    }
+    
     
 }
