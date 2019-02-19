a6867944e585ea11cec614e86985860c47b7ec3b
Calcanas <43923999+Calcanas@users.noreply.github.com>
2019-02-19 00:34:01 -0600

    Update Compra.java

--- AVCM/src/model/Compra.java
+++ AVCM/src/model/Compra.java
@@ -6,9 +6,84 @@
 package model;
 
 /**
- *
- * @author diegg
+ * Version: 1 
+ * @author Mónica del Rosario Díaz Arredondo
+ * Fecha: 16 - 02 - 2019
+ * Descripción: Modelado de clase Compra
  */
 public class Compra {
+    private String fechaCompra;
+    private int idProducto;
+    private int idVendedor;
+    private int idAlmacen;
+    private int idProveedor;
+    private float importe;
+    private int status;
+    private String fechaPago;
+
+    public String getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(String fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public int getIdAlmacen() {
+        return idAlmacen;
+    }
+
+    public void setIdAlmacen(int idAlmacen) {
+        this.idAlmacen = idAlmacen;
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public float getImporte() {
+        return importe;
+    }
+
+    public void setImporte(float importe) {
+        this.importe = importe;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(String fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+    
     
 }
