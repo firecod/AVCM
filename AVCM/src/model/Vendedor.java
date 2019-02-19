f47d231252830495e931b6c077a6c7adeef75321
Calcanas <43923999+Calcanas@users.noreply.github.com>
2019-02-19 00:32:00 -0600

    Update Vendedor.java

--- AVCM/src/model/Vendedor.java
+++ AVCM/src/model/Vendedor.java
@@ -6,9 +6,66 @@
 package model;
 
 /**
- *
- * @author diegg
+ * Version: 1
+ * @author: Mónica del Rosario Díaz Arredondo
+ * Fecha: 16 - 02 -2019
+ * Descripción: Modelado de la clase Vendedors
  */
 public class Vendedor {
+    private int id;
+    private String numeroVendedor;
+    private String fotografíaVendedor;
+    private int reputacion;
+    private int estatus;
+    private int idPersona;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumeroVendedor() {
+        return numeroVendedor;
+    }
+
+    public void setNumeroVendedor(String numeroVendedor) {
+        this.numeroVendedor = numeroVendedor;
+    }
+
+    public String getFotografíaVendedor() {
+        return fotografíaVendedor;
+    }
+
+    public void setFotografíaVendedor(String fotografíaVendedor) {
+        this.fotografíaVendedor = fotografíaVendedor;
+    }
+
+    public int getReputacion() {
+        return reputacion;
+    }
+
+    public void setReputacion(int reputacion) {
+        this.reputacion = reputacion;
+    }
+
+    public int getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(int estatus) {
+        this.estatus = estatus;
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+    
     
 }
